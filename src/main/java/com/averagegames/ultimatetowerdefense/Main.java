38d4397275729ad4dbe6c6509169144853c4311d
Luke Palguta <lpalguta@sewickley.org>
2025-02-25 17:32:15 -0500

    There was a missed compiler error in the previous commit. That is the only change that has been made.

--- src/main/java/com/averagegames/ultimatetowerdefense/Main.java
+++ src/main/java/com/averagegames/ultimatetowerdefense/Main.java
@@ -1,6 +1,5 @@
 package com.averagegames.ultimatetowerdefense;
 
-import static com.averagegames.ultimatetowerdefense.Manager.LOGGER;
 import static com.averagegames.ultimatetowerdefense.scenes.SceneBuilder.loadBuild;
 
 import com.averagegames.ultimatetowerdefense.scenes.GameScene;
