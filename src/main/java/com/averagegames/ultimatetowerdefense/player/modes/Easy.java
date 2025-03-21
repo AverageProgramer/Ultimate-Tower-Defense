00610ddf3c11ba9df2a0f9246a2222d63bbee617
Luke Palguta <lpalguta@sewickley.org>
2025-03-20 20:54:51 -0400

    Removed the Soldier Enemies from the first Wave in the Easy class.

--- src/main/java/com/averagegames/ultimatetowerdefense/player/modes/Easy.java
+++ src/main/java/com/averagegames/ultimatetowerdefense/player/modes/Easy.java
@@ -9,10 +9,6 @@ import com.averagegames.ultimatetowerdefense.characters.enemies.survival.zombies
 
 public class Easy {
     public static final Wave WAVE_1 = new Wave(new Enemy[] {
-            new Soldier(),
-            new Soldier(),
-            new Soldier(),
-
             new Normal(),
             new Normal(),
             new Normal(),
